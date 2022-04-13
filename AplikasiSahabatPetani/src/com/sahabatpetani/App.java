bc96882b89ad7410dd300b27e68f61ac0a0b84e9
itsdicky <dickysatria.2020@student.uny.ac.id>
2022-04-13 14:50:40 +0700

    Initial commit.

---
+++ AplikasiSahabatPetani/src/com/sahabatpetani/App.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.sahabatpetani;
+
+/**
+ *
+ * @author HP PAVILION
+ */
+public class App {
+    public static void main(String[] args) {
+        //run app
+    }
+}
